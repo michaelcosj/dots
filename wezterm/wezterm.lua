b74e1169e11d58ec9b003cc1241bfdfdb711153a
michael <michaelcosj@proton.me>
2024-03-22 10:32:21 +0100

    edits and added wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+-- Apperance
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.color_scheme = 'ayu'
+
+-- Tab bar
+config.enable_tab_bar = false
+
+return config
