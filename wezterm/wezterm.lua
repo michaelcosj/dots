8f4f49c6caf908e5dbc6d64f826562a1226243c0
Michael Osajeh <michaelcosj@proton.me>
2024-07-25 22:17:07 +0100

    minor config updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,9 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- Apperance
-config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'ayu'
+config.window_background_opacity = 1
 
 -- Tab bar
 config.enable_tab_bar = false
